11c19a68280f4df86ba8ec5b872faa89df870ec5
zhaobingwang <zhaobingwang@foxmail.com>
2020-11-16 17:35:15 +0800

    feat: 新增JWT接口

---
+++ Template/Template.RBAC/Template.RBAC.WebApi/Config/AppAuthenticationSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Template.RBAC.WebApi.Config
+{
+    /// <summary>
+    /// 应用认证配置
+    /// </summary>
+    public class AppAuthenticationSettings
+    {
+        public string AppId { get; set; }
+        public string Secret { get; set; }
+    }
+}
