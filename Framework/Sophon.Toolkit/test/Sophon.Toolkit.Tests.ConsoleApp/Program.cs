b5c0edf14cc6751160b5530dad9bb1b98c5a4685
zhaobingwang <zhaobingwang@foxmail.com>
2020-12-16 14:38:20 +0800

    feat: 新增system工具类

---
+++ Framework/Sophon.Toolkit/test/Sophon.Toolkit.Tests.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sophon.Toolkit.Tests.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var memoryMetrics = SystemUtil.GetMemoryMetrics();
+            Console.WriteLine($"{memoryMetrics.Total}\t{memoryMetrics.Used}\t{memoryMetrics.Free}");
+        }
+    }
+}
