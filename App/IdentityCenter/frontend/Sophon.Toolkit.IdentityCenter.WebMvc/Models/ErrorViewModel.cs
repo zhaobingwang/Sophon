75f452464a3b0b613f83007b79d96e09118f89ef
zhaobingwang <zhaobingwang@foxmail.com>
2020-12-29 15:37:29 +0800

    initialize Sophon.Toolkit.IdentityCenter

---
+++ App/IdentityCenter/frontend/Sophon.Toolkit.IdentityCenter.WebMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sophon.Toolkit.IdentityCenter.WebMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
