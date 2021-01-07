8b3b28455d4a27cdf1cfcad96f38672413e02dc5
zhaobingwang <zhaobingwang@foxmail.com>
2021-01-07 13:37:18 +0800

    feat: add CryptographyUtil

---
+++ Framework/Sophon.Toolkit/src/Sophon.Toolkit/Sophon/Util/CryptographyUtil.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sophon.Toolkit
+{
+    /// <summary>
+    /// 密码学工具类
+    /// </summary>
+    public static partial class CryptographyUtil
+    {
+        /// <summary>
+        /// 获取MD5哈希值，默认编码为UTF8
+        /// </summary>
+        /// <param name="value">待计算的字符串</param>
+        /// <returns>32位大写哈希字符串</returns>
+        public static string GetMd5Hash(string value, bool toUpper = true)
+        {
+            return GetMd5Hash(value, Encoding.UTF8, toUpper);
+        }
+
+        /// <summary>
+        /// 获取MD5哈希值
+        /// </summary>
+        /// <param name="value">待计算的字符串</param>
+        /// <param name="encoding">编码方式</param>
+        /// <returns>32位大写哈希字符串</returns>
+        public static string GetMd5Hash(string value, Encoding encoding, bool toUpper = true)
+        {
+            if (value.IsNullOrWhiteSpace())
+                return string.Empty;
+
+            using (var md5 = MD5.Create())
+            {
+                var hashBytes = md5.ComputeHash(encoding.GetBytes(value));
+                var hash = BitConverter.ToString(hashBytes);
+
+                if (toUpper)
+                    return hash.Replace("-", "");
+                else
+                    return hash.ToLower().Replace("-", "");
+            }
+        }
+    }
+}
---
+++ Framework/Sophon.Toolkit/test/Sophon.Toolkit.Tests/Util/CryptographyUtilTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sophon.Toolkit.Tests.Util
+{
+    [Trait("工具类", "密码学")]
+    public class CryptographyUtilTest
+    {
+        [Fact(DisplayName = "获取大写MD5值-正常参数测试")]
+        public void GetUppercaseMd5_ShouldSuccess_WithExpectedParameters()
+        {
+            // Arrange
+            var source = "123456";
+            var expected = "E10ADC3949BA59ABBE56E057F20F883E";
+            // Act
+            var actual = CryptographyUtil.GetMd5Hash(source);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact(DisplayName = "获取MD5值-异常参数测试")]
+        public void GetUppercaseMd5_ShouldSuccess_WithUnExpectedParameters()
+        {
+            // Arrange
+            var source = "";
+            var expected = string.Empty;
+            // Act
+            var actual = CryptographyUtil.GetMd5Hash(source);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact(DisplayName = "获取小写MD5值-正常参数测试")]
+        public void GetLowercaseMd5_ShouldSuccess_WithExpectedParameters()
+        {
+            // Arrange
+            var source = "123456";
+            var expected = "e10adc3949ba59abbe56e057f20f883e";
+            // Act
+            var actual = CryptographyUtil.GetMd5Hash(source, Encoding.UTF8, false);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
