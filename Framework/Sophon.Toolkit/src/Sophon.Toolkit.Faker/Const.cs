f677c8fc27cc1971fce36ea276560f7e2bac409d
zhaobingwang <zhaobingwang@foxmail.com>
2021-01-11 17:19:06 +0800

    临时提交

---
+++ Framework/Sophon.Toolkit/src/Sophon.Toolkit.Faker/Const.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sophon.Toolkit.Faker
+{
+    public class Const
+    {
+        public const string LANG_ZH_CN = "zh_CN";
+    }
+}
---
+++ Framework/Sophon.Toolkit/src/Sophon.Toolkit.Faker/UserFaker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sophon.Toolkit.Faker
+{
+    public class UserFaker
+    {
+        private readonly Bogus.DataSets.Name _name;
+        private readonly Bogus.DataSets.PhoneNumbers _phoneNumbers;
+        public UserFaker(string locale = Const.LANG_ZH_CN)
+        {
+            _name = new Bogus.DataSets.Name(locale);
+            _phoneNumbers = new Bogus.DataSets.PhoneNumbers(locale);
+        }
+
+        /// <summary>
+        /// 获取一个中文姓名
+        /// </summary>
+        /// <returns>中文姓名</returns>
+        public string GetName()
+        {
+            var firstName = _name.FirstName();
+            var lastName = _name.LastName();
+            return $"{lastName}{firstName}";
+        }
+
+        /// <summary>
+        /// 获取一个手机/电话号码
+        /// </summary>
+        /// <returns></returns>
+        public string GetOnePhoneNumber()
+        {
+            return _phoneNumbers.PhoneNumber();
+        }
+
+    }
+}
