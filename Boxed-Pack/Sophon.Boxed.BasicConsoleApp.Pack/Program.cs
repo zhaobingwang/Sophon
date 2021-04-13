c46ae50ac6baceec3db8563349688017a9029bed
zhaobingwang <zhaobingwang@foxmail.com>
2021-04-13 17:00:32 +0800

    feat: dotnet new templates

---
+++ Boxed-Pack/Sophon.Boxed.BasicConsoleApp.Pack/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sophon.Boxed.BasicConsoleApp.Pack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Boxed-Pack/Sophon.Boxed.BasicWebApi.Pack/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sophon.Boxed.BasicWebApi.Pack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
